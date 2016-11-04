66673d0e1d8f4b29248f67c1a0a9b286d7f74d6e
Albert Costas <albert@adsmurai.com>
2016-11-04 11:41:39 +0000

    Initial commit

---
+++ Features/Bootstrap/FeatureContext.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Features\Bootstrap;
+
+use Behat\MinkExtension\Context\MinkContext;
+
+class FeatureContext extends MinkContext
+{
+}
---
+++ Features/loginHome.feature
@@ -0,0 +1,5 @@
+Feature: ls
+  In has katana user
+
+  Scenario:
+    Given I should be on the homepage
